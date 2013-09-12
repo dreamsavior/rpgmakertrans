b3fffd5ca92f7043eab4e9d4150af2d454d194be
habisain <habisain@gmail.com>
2013-09-12 21:53:00 +0100

    Support for more complex patch file structures - will be necessary for v3
    --HG--
    branch : rpgmakertransv2

--- patchers/filepatcher.py
+++ patchers/filepatcher.py
@@ -26,16 +26,13 @@ class FilePatcher(BasePatch):
     def makeTranslator(self):
         data = {}
         mtime = 0
-        for fn in os.listdir(self.path):
-            if fn.lower().endswith('.txt'):
-                fullfn = os.path.join(self.path, fn)
-                name = fn.rpartition('.')[0].lower()
-                mtime = max(mtime, os.path.getmtime(fullfn))
-            try:
-                with codecs.open(fullfn, 'r', encoding='utf-8') as f:
-                    data[name] = f.read()
-            except UnicodeError:
-                pass # Not a valid RPGMaker Trans file, evidently.
+        self.genPatchDataFiles()
+        for fn in self.patchDataFiles:
+            name = os.path.split(fn)[1].rpartition('.')[0].lower()
+            mtime = max(mtime, os.path.getmtime(fn))
+            with codecs.open(fn, 'r', encoding='utf-8') as f:
+                data[name] = f.read()
+            
         return getattr(self.translatorManager, type(self).translatorClass)(data, mtime)
     
     def writeTranslator(self, translator, path=None):
@@ -46,6 +43,21 @@ class FilePatcher(BasePatch):
             fullfn = os.path.join(path, fn)
             with codecs.open(fullfn, 'w', encoding='utf-8') as f:
                 f.write(data[name])
+                
+    def __patchDataFiles(self):
+        for fn in self.__filePaths():
+            if fn.endswith('.txt'):
+                try:
+                    with codecs.open(fn, 'r', encoding='utf-8') as f:
+                        header = '# RPGMAKER TRANS PATCH'
+                        x = f.read(len(header))
+                        if x.startswith(header):
+                            yield fn
+                except UnicodeError:
+                    pass
+                
+    def genPatchDataFiles(self):
+        self.patchDataFiles = [x for x in self.__patchDataFiles()]
     
     def __filePaths(self):
         for dir, subdir, files in os.walk(self.path):
@@ -93,15 +105,22 @@ class FilePatcherv2(FilePatcher):
     translatorClass = 'Translator2kv2'
     
 def sniffv2(path):
-    if os.path.isfile(path) and path.endswith('rpgmktranspatch'):
-        path = os.path.split(path)[0]
     if os.path.isdir(path):
-        path = os.path.join(path, 'Map0001.txt')
+        path = os.path.join(path, 'rpgmktranspatch')
     if os.path.isfile(path):
         with open(path, 'r') as f:
-            content = f.read()
-        if content.startswith('# RPGMAKER TRANS PATCH FILE VERSION 2.0'):
+            versionString = f.read()
+        if not versionString.strip():
             return True
+    #if os.path.isfile(path) and path.endswith('rpgmktranspatch'):
+    #    path = os.path.split(path)[0]
+    #if os.path.isdir(path):
+    #    path = os.path.join(path, 'Map0001.txt')
+    #if os.path.isfile(path):
+    #    with open(path, 'r') as f:
+    #        content = f.read()
+    #    if content.startswith('# RPGMAKER TRANS PATCH FILE VERSION 2.0'):
+    #        return True
     return False         
         
 filePatchers[sniffv2] = FilePatcherv2
