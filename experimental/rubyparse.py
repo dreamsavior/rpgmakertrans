46c1b83ada25d1d490b8ee44fb89d74a6785fde6
Aleph Fell <habisain@gmail.com>
2014-06-28 01:50:21 +0100

    Add docs to experimental module
    --HG--
    branch : rpgmakertrans

--- experimental/rubyparse.py
+++ experimental/rubyparse.py
@@ -1,12 +1,16 @@
-'''
-Created on 16 May 2014
-
-@author: habisain
-'''
-
 """
-There are essentially two contexts: strings / non strings.
+rubyparse
+*********
+
+:author: Aleph Fell <habisain@gmail.com>
+:copyright: 2012-2014
+:license: GNU Public License version 3
+
+The experimental Ruby parser. This is written in Python because Ruby1.8 lacks
+string encoding awareness. As Ruby scripts can have 1 of 4 encodings, this
+creates substantial problems for parsing Ruby in Ruby.
 """
+
 
 
 class Rule:
