95a3a545964bebc1483e47f3227ffd391084aa04
Aleph Fell <habisain@gmail.com>
2014-12-10 16:32:42 +0000

    Tidy up and document interface (not its submodule QTUI)
    --HG--
    branch : add-vx-support

--- rpgmakertransv2/interface/cli.py
+++ rpgmakertransv2/interface/cli.py
@@ -15,14 +15,16 @@ import sys
 import argparse
 from ..controllers.coreprotocol import CoreRunner, CoreProtocol
 from ..controllers.headless import Headless
+from ..version import version
 
-VERSION = 2.0
+VERSION = version
 CLI_LENGTH = 79
 
 
 class CLIMode(CoreProtocol):
-
+    """The CLI Mode. Much simpler than the GUI mode, to be blunt"""
     def __init__(self, cargs, *args, **kwargs):
+        """Initialise the CLI mode, with given CLI arguments"""
         super(CLIMode, self).__init__(*args, **kwargs)
         # TODO: Need to check if the arguments are actually valid.
         self.quiet = cargs.quiet
@@ -31,37 +33,42 @@ class CLIMode(CoreProtocol):
         self.normalPrint('Patch path : %s' % cargs.patch)
         self.normalPrint('Output path: %s' % cargs.output)
         self.progressPrint('Starting patcher...')
-        self.headless = self.runner.initialise(
-            Headless,
-            outputcoms=self.inputcoms)
+        self.headless = self.runner.initialise(Headless,
+                                               outputcoms=self.inputcoms)
         self.runner.attach(self.headless)
         self.headless.go(cargs.input, cargs.patch, cargs.output)
 
     def errorMsgQuit(self, string):
+        """Write an error message to stderr"""
         sys.stderr.write(string)
         sys.stderr.flush()
         sys.exit(1)
 
     def normalPrint(self, string):
+        """Print something if we're supposed to print"""
         if not self.quiet:
             print(string)
 
     def progressPrint(self, string):
+        """Print something using a progress bar style print"""
         if not self.quiet:
             print(string.ljust(CLI_LENGTH) + '\r', end=' ')
             sys.stdout.flush()
 
     def setProgress(self, progress):
+        """Set the current progress"""
         hashes = ('#' * int(70 * progress)).ljust(70)
         percent = str(int(progress * 100)).ljust(3)
         self.progressPrint('[%s] %s %%' % (hashes, percent))
 
     def finishedPatching(self):
+        """Finish patching"""
         self.normalPrint('\nPatching finished')
         self.going = False
 
 
 def useCLIBackend():
+    """Function to determine if we should use the CLI backend"""
     longargs = set()
     shortargs = set()
     for x in sys.argv:
@@ -77,23 +84,17 @@ def useCLIBackend():
 
 
 def CLIBackend(runner):
+    """Function to run the CLI Backend"""
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-c',
-        '--cli',
-        help='Enable CLI mode',
-        action='store_true')
+    parser.add_argument('-c', '--cli', help='Enable CLI mode', 
+                        action='store_true')
     parser.add_argument("input", help="Path of input game to patch")
     parser.add_argument("patch", help="Path of patch (directory or zip)")
-    parser.add_argument(
-        "output",
-        help="Path to output directory (will create missing directories)")
-    parser.add_argument(
-        '-q',
-        '--quiet',
-        help='Suppress all output',
-        action='store_true')
-    t = sys.stderr
+    parser.add_argument("output",
+                        help="Path to output directory (will create)")
+    parser.add_argument('-q', '--quiet', help='Suppress all output',
+                        action='store_true')
+    t = sys.stderr # Hacks to ensure that custom error handling is suppressed
     sys.stderr = sys.__stderr__
     args = parser.parse_args()
     sys.stderr = t
--- rpgmakertransv2/interface/guicontroller.py
+++ rpgmakertransv2/interface/guicontroller.py
@@ -22,8 +22,12 @@ from ..version import versionCheck
 
 
 class IDStore(dict):
-
+    """IDStore is basically a dict which also keeps track
+    of a unique ID that can be added into the IDStore, as well
+    as the inverse dict"""
     def __init__(self, reverse=True, *args, **kwargs):
+        """Initialise hte ID store, creating an inverse dict if 
+        reverse=True"""
         super(IDStore, self).__init__(*args, **kwargs)
         self.nextid = 0
         self.reversable = reverse
@@ -31,11 +35,13 @@ class IDStore(dict):
             self.reverse = {}
 
     def __setitem__(self, key, val):
+        """Update the inverse dict if necessary"""
         if self.reversable:
             self.reverse[val] = key
-        super(IDStore, self).__setitem__(key, val)
+        super().__setitem__(key, val)
 
     def add(self, item):
+        """Add an item to the next available ID"""
         r = self.nextid
         self[item] = r
         self.nextid += 1
@@ -43,26 +49,32 @@ class IDStore(dict):
 
 
 class UpdaterDict(dict):
-
+    """Dictionary that runs a given function when an item is set"""
     def __init__(self, updateFunc, *args, **kwargs):
+        """Initialise the updater dict"""
         self.__updateFunc = updateFunc
         super(UpdaterDict, self).__init__(*args, **kwargs)
 
     def __setitem__(self, key, value):
+        """Update the dict, then run the updater function"""
         super(UpdaterDict, self).__setitem__(key, value)
         self.__updateFunc()
 
 
 class GUIController(CoreProtocol):
-
+    """The GUI Controller, which handles communications between
+    a GUI and the backend. Mainly a translator between headless
+    and a concrete GUI like QT.
+    NOTE: At some point, it may be prudent to do something like
+    make the GUI be a subclass of GUIController. Creating both
+    the controller and the GUI in two different processes seems
+    unnecessary."""
     def __init__(self, *args, **kwargs):
+        """Initialise and start the GUI controller"""
         super(GUIController, self).__init__(*args, **kwargs)
         self.setupPool('gui', processes=1)
-        self.submit(
-            'gui',
-            startView,
-            inputcoms=self.outputcoms,
-            outputcoms=self.inputcoms)
+        self.submit('gui', startView, inputcoms=self.outputcoms,
+                    outputcoms=self.inputcoms)
         self.runner.setErrorHandler(errorMsg)
         self.gameDB = IDStore()
         self.patchDB = IDStore()
@@ -79,34 +91,29 @@ class GUIController(CoreProtocol):
 
         self.outputcoms.send('setMessage', 'Loading games, patches...')
         self.setupPool('worker', processes=1)
-        sniffDataRet = self.submit(
-            'worker',
-            sniffAllTrigger,
-            path=os.getcwd(),
-            coms=self.inputcoms)
+        sniffDataRet = self.submit('worker', sniffAllTrigger, 
+                                   path=os.getcwd(), coms=self.inputcoms)
         self.submit('worker', versionCheck, coms=self.inputcoms)
-        self.localWaitUntil(
-            'sniffingDone',
-            self.setUpSniffedData,
-            sniffDataRet)
+        self.localWaitUntil('sniffingDone', self.setUpSniffedData, 
+                            sniffDataRet)
 
     def newVerAvailable(self, version):
-        self.outputcoms.send(
-            'displayOKKill',
-            'New Version Available',
-            'Version %s of RPGMaker Trans is available.\nPlease visit http://habisain.blogspot.co.uk to download it.\nPress OK to close RPGMaker Trans' %
-            str(
-                round(
-                    version,
-                    2)))
+        """Display a message+quit if new version available"""
+        self.outputcoms.send('displayOKKill', 'New Version Available',
+            'Version %s of RPGMaker Trans is available.\n'
+            'Please visit http://habisain.blogspot.co.uk to download it.\n'
+            'Press OK to close RPGMaker Trans' %
+            str(round(version,2)))
 
     def expired(self):
-        self.outputcoms.send(
-            'displayOKKill',
-            'Expired Version',
-            'This version of RPGMaker Trams has expired.\nPlease visit http://habisain.blogspot.co.uk to download a new version.\nPress OK to close RPGMaker Trans')
+        """Display a message+quit if too old"""
+        self.outputcoms.send('displayOKKill', 'Expired Version',
+            'This version of RPGMaker Trams has expired.'
+            'Please visit http://habisain.blogspot.co.uk to download a new version.'
+            'Press OK to close RPGMaker Trans')
 
     def setUpSniffedData(self, sniffDataRet):
+        """Put sniffed data into the GUI"""
         sniffData = sniffDataRet.get()
         for item in sniffData:
             if item is False:
@@ -119,14 +126,9 @@ class GUIController(CoreProtocol):
                 self.addTrans(item, select=False)
         self.outputcoms.send('setMessage', 'Ready')
 
-    def addItem(
-            self,
-            sniffData,
-            sniffDataTypes,
-            idstore,
-            signalSuffix,
-            select,
-            prefix=None):
+    def addItem(self, sniffData, sniffDataTypes, idstore,
+                signalSuffix, select, prefix=None):
+        """This is the general 'add item to selector' logic"""
         # Take care of stuff where we can't do anything...
         # if sniffData is None:
         #    sniffData = sniff(path, positives=sniffDataTypes)
@@ -143,82 +145,54 @@ class GUIController(CoreProtocol):
             name = '%s %s' % (prefix, name)
         if path not in idstore:
             tid = idstore.add(path)
-            self.outputcoms.send(
-                'add%s' %
-                signalSuffix,
-                name,
-                tid,
-                select=select)
+            self.outputcoms.send('add%s' % signalSuffix, name,
+                                 tid, select=select)
         else:
             tid = idstore[path]
             if select:
                 self.outputcoms.send('select%s' % signalSuffix, tid)
 
-    def addItemFromPath(
-            self,
-            path,
-            sniffDataTypes,
-            idStore,
-            signalSuffix,
-            select=False,
-            prefix=None):
+    def addItemFromPath(self, path, sniffDataTypes, idStore, signalSuffix,
+                        select=False, prefix=None):
+        """Add an item from path"""
         sniffData = sniff(path)  # , positives=sniffDataTypes)
         for item in sniffData:
-            self.addItem(
-                item,
-                sniffDataTypes,
-                idStore,
-                signalSuffix,
-                select,
-                prefix)
+            self.addItem(item, sniffDataTypes, idStore,
+                         signalSuffix, select, prefix)
 
     def addGame(self, sniffData, select=False):
-        self.addItem(
-            sniffData, [
-                'GAME', 'TRANS'], self.gameDB, 'Game', select, prefix='[%s]')
+        """Add a game to UI"""
+        self.addItem(sniffData, ['GAME', 'TRANS'], self.gameDB, 'Game',
+                     select, prefix='[%s]')
 
     def addGameFromPath(self, gamepath, select=False):
-        self.addItemFromPath(
-            gamepath, [
-                'GAME', 'TRANS'], self.gameDB, 'Game', select, prefix='[%s]')
+        """Add Game from path"""
+        self.addItemFromPath(gamepath, ['GAME', 'TRANS'], self.gameDB, 'Game',
+                             select, prefix='[%s]')
 
     def addPatch(self, sniffData, select=False):
-        self.addItem(
-            sniffData,
-            ['PATCH'],
-            self.patchDB,
-            'Patch',
-            select,
-            prefix='[%s]')
+        """Add patch to UI"""
+        self.addItem(sniffData, ['PATCH'], self.patchDB, 'Patch', select,
+                     prefix='[%s]')
 
     def addPatchFromPath(self, patchpath, select=False):
-        self.addItemFromPath(
-            patchpath,
-            ['PATCH'],
-            self.patchDB,
-            'Patch',
-            select,
-            prefix='[%s]')
+        """Add patch from path to UI"""
+        self.addItemFromPath(patchpath, ['PATCH'], self.patchDB, 'Patch', 
+                             select, prefix='[%s]')
 
     def addTrans(self, sniffData, select=False):
-        self.addItem(
-            sniffData,
-            ['TRANS'],
-            self.transDB,
-            'Trans',
-            select,
-            prefix='[%s]')
+        """Add translation to UI"""
+        self.addItem(sniffData, ['TRANS'], self.transDB, 'Trans', select,
+                     prefix='[%s]')
 
     def addTransFromPath(self, transpath, select=False):
-        self.addItemFromPath(
-            transpath,
-            ['TRANS'],
-            self.transDB,
-            'Trans',
-            select,
-            prefix='[%s]')
+        """Add translation from path"""
+        self.addItemFromPath(transpath, ['TRANS'], self.transDB, 'Trans',
+                             select, prefix='[%s]')
 
     def selectDefaultPatch(self):
+        """Given a game, figure out if we know it's default patch and
+        select it"""
         if self.currentState['gameloc'] is None:
             return
         gamepath = self.gameDB.reverse[self.currentState['gameloc']]
@@ -239,6 +213,7 @@ class GUIController(CoreProtocol):
                 self.addPatch(item, select=True)
 
     def changeSelected(self, idtoken, newid):
+        """Update state when something changes"""
         self.currentState[idtoken] = newid
         if idtoken == 'gameloc':
             self.selectDefaultPatch()
@@ -249,12 +224,15 @@ class GUIController(CoreProtocol):
                 self.addTransFromPath(defaulttranspath, select=True)
 
     def optionChanged(self, option, value):
+        """Update state when an option changes"""
         self.currentState[option] = value
         if option == 'create':
             self.selectDefaultPatch()
             self.outputcoms.send('setBrowsePatchDirs', value)
 
     def enableUI(self):
+        """Enable/disable the UI based on current state. Automatically
+        called when state changes"""
         state = self.currentState['enabled']
         states = {}
         states['gameloc'] = state
@@ -268,6 +246,8 @@ class GUIController(CoreProtocol):
         self.outputcoms.send('setUI', states)
 
     def go(self):
+        """The big GO button that starts the translation process
+        based on current state"""
         self.outputcoms.send('resetNonfatalError')
         gamepath = self.gameDB.reverse[self.currentState['gameloc']]
         patchpath = self.patchDB.reverse[self.currentState['patchloc']]
@@ -279,26 +259,31 @@ class GUIController(CoreProtocol):
         self.outputcoms.send('setMessage', 'Patching game...')
 
     def nonfatalError(self, msg):
+        """Display a non fatal error message"""
         self.outputcoms.send('nonfatalError', msg)
 
     def finalisingPatch(self):
+        """Display the finalising patch message"""
         self.outputcoms.send('setMessage', 'Finalising Patch')
 
     def finishedPatching(self):
+        """Update UI for finishing patch"""
         self.currentState['enabled'] = True
         self.currentState['gameloc'] = None
         self.outputcoms.send('setMessage', 'Finished patching')
-        # self.enableUI()
 
     def abortPatching(self):
+        """Update UI when aborting patch"""
         self.currentState['enabled'] = True
         self.currentState['gameloc'] = None
         self.outputcoms.send('setMessage', 'Patching aborted')
 
     def setProgress(self, amount):
+        """Set the progress of the UI"""
         self.outputcoms.send('setProgress', amount)
 
     def stop(self):
+        """Do a 'really quit' box if patching"""
         if self.headless is None or not self.headless.going:
             self.outputcoms.send('quit')
         else:
@@ -308,10 +293,12 @@ class GUIController(CoreProtocol):
                                  'Really quit?', yes='reallyStop')
 
     def reallyStop(self):
+        """Really quit the patching if asked"""
         self.outputcoms.send('quit')
 
 
 def sniffAllTrigger(path, coms):
+    """Run sniffAll, send appropriate trigger when done"""
     ret = sniffAll(path)
     coms.send('trigger', 'sniffingDone')
     return ret
