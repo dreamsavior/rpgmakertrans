08affa81340f56709bea80e5023597bea6ef2b79
Aleph Fell <habisain@gmail.com>
2014-12-10 16:00:17 +0000

    More rewriting away from fileops and also documentation in the mtimesmanager file
    --HG--
    branch : add-vx-support

--- rpgmakertransv2/workers/mtimesmanager.py
+++ rpgmakertransv2/workers/mtimesmanager.py
@@ -9,48 +9,56 @@ mtimesmanager
 A multiprocess capable handler for the mtimes file, which is used
 to try and not redo everything with every single patch.
 """
+
 from ..errorhook import errorWrap
 import os.path
 import pickle
 from ..metamanager import CustomManager, MetaCustomManager
 import multiprocessing
-from .fileops import WinOpen
 
 
 class MTimesHandlerManager(CustomManager):
-    pass
-
+    """Manager for MTimesHandler"""
 
 class MetaMTimesManager(MetaCustomManager):
+    """Metaclass for automatic registering of MTimesHandler"""
     customManagerClass = MTimesHandlerManager
 
-
 class MTimesHandler(object, metaclass=MetaMTimesManager):
-
+    """Provides a nice clean way to track the modification times of
+    files being patched. There is a distinction between the current
+    mtimes and the ovserved (new) mtimes, which means that should there 
+    be a high turnover of files, old mtimes won't clutter up the mtimes
+    database."""
     def __init__(self, outpath):
+        """Initialise an mtimes manager for target directory outpath"""
         self.mtimespath = os.path.join(outpath, 'mtimes')
         self.manager = multiprocessing.Manager()
         self.mtimes = self.manager.dict()
         self.newmtimes = self.manager.dict()
 
     def loadMTimes(self):
+        """Try to load mtimes. If fail, assume no mtimes available"""
         try:
-            with WinOpen(self.mtimespath) as f:
+            with open(self.mtimespath) as f:
                 loadedmtimes = pickle.load(f)
         except:
             loadedmtimes = {}
         self.mtimes.update(loadedmtimes)
 
     def dumpMTimes(self):
+        """Dump *new* mtimes to output file"""
         dumpmtimes = {}
         dumpmtimes.update(self.newmtimes)
-        with WinOpen(self.mtimespath, 'wb') as f:
+        with open(self.mtimespath, 'wb') as f:
             pickle.dump(dumpmtimes, f)
 
     def getMTimes(self):
+        """Return multiprocess capable mtimes dictionary"""
         return self.mtimes
 
     def getNewMTimes(self):
+        """Return multiprocess capable newmtimes dictionary""" 
         return self.newmtimes
 
 
