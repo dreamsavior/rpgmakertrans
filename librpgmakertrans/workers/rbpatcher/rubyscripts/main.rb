e9d72c499cff5b149a3c06650df7ec50bd5f300f
Aleph Fell <habisain@gmail.com>
2014-12-13 23:47:59 +0000

    Added a new 'universal' loader scripts, which should in theory work on XP/VX and VXAce.
    --HG--
    branch : add-vx-support

---
+++ librpgmakertrans/workers/rbpatcher/rubyscripts/main.rb
@@ -0,0 +1,10 @@
+# rbmain.rb
+# *********
+#
+# :author: Aleph Fell <habisain@gmail.com>
+# :copyright: 2012-2014
+# :license: GNU Public License version 3
+#
+# Provides the main loop for the Ruby patching engine
+#
+
