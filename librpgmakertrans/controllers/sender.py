bbe79119ded5a0501f1655fcfa816096141fa668
Aleph Fell <habisain@gmail.com>
2014-12-27 15:02:01 +0000

    Added IDs to Senders
    --HG--
    branch : add-vx-support

--- librpgmakertrans/controllers/sender.py
+++ librpgmakertrans/controllers/sender.py
@@ -8,37 +8,59 @@ sender
 
 Sender is a simple multiprocessing method of sending signals
 """
+
 import sys
+import uuid
 
 from multiprocessing.managers import BaseManager
 
-
 class SenderManager(BaseManager):
-    pass
+    """Manager for Sender objects"""
 
+class SenderID:
+    """ID for Senders"""
+    def __init__(self):
+        """Setup the ID"""
+        self.uuid = uuid.uuid4()
+        
+    def __hash__(self):
+        """Hash function"""
+        return hash(type(self), self.uuid)
+    
+    def __eq__(self, other):
+        """Equality operator"""
+        return type(self) == type(other) and self.uuid == other.uuid
 
 class Sender:
-
+    """A Sender for data between processes"""
     def __init__(self):
+        """Initialise the Sender"""
         self.__signals = []
+        self.senderID = SenderID()
 
     def send(self, signal, *args, **kwargs):
+        """Send a signal"""
         self.__signals.append((signal, args, kwargs))
 
     def get(self):
+        """Get the signals in the Sender"""
         ret = self.__signals
         self.__signals = []
         return ret
 
     def __getattr__(self, key):
+        """Method for remote calling - depreceated"""
         def wrap(self, *args, **kwargs):
+            """Wrap the send function with the key"""
             self.send(key, *args, **kwargs)
         wrap.__name__ = 'send%s' % key
         self.key = wrap
         return self.key
     
 class ErrorSender(Sender):
+    """Version of Sender which dumps all signals to stderr"""
     def send(self, signal, *args, **kwargs):
+        """Put signals onto stderr, with special handling for errors"""
         if signal == 'ERROR':
             if len(args) > 0:
                 print(args[0], file=sys.stderr)
