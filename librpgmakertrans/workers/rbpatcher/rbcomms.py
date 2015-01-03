33a02d5594471a6abf5ab5409d8791738cacea5a
Aleph Fell <habisain@gmail.com>
2015-01-03 10:41:08 +0000

    Enable multiple ruby processes
    --HG--
    branch : add-vx-support

--- librpgmakertrans/workers/rbpatcher/rbcomms.py
+++ librpgmakertrans/workers/rbpatcher/rbcomms.py
@@ -14,6 +14,7 @@ import asyncio
 import os
 import subprocess
 from collections import OrderedDict
+import multiprocessing
 
 from ...controllers.socketcomms import SocketComms
 from librpgmakertrans.errorhook import errorWrap
@@ -183,7 +184,7 @@ def startRBComms(indir, outdir, translator, mtimes, newmtimes,
     the data directories (todo: recursive approach)"""
     filesToProcess = RBComms.makeFilesToProcess(indir, outdir)
     rpgversion = 'vx'
-    subprocesses = 1
+    subprocesses = multiprocessing.cpu_count()
     rbcomms = RBComms(translator, filesToProcess, rpgversion, inputComs,
                       outputComs, subprocesses)
     rbcomms.start()
