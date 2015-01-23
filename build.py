cf0ae48f8cef3026ac5f7fd5b59876131fb8d1e5
Aleph Fell <habisain@gmail.com>
2015-01-24 04:48:51 +0900

    Fixed build script
    --HG--
    branch : add-vx-support

--- build.py
+++ build.py
@@ -13,22 +13,34 @@ or if I should embed the SVG into a Python file or something.
 """
 
 import sys
-
+import os
 from cx_Freeze import setup, Executable
-from rpgmakertransv2.version import version
+from librpgmakertrans.version import version
 
 base = None
+
+build_exe_options = {
+"include_files": [(os.path.join('librpgmakertrans', 'workers', 'rbpatcher', 'pruby'),
+                   'pruby'),
+                  (os.path.join('librpgmakertrans', 'workers', 'rbpatcher', 'rubyscripts'),
+                   'rubyscripts'),]
+}
+
 setup(
-    name="RPGMaker Trans CLI" % version,
+    name="RPGMaker Trans CLI %s" % version,
     version="%s" % version,
     description="Translation tool for RPGMaker games, CLI mode",
-    executables=[Executable("rpgmakertrans_cli.py", base=base)])
+    executables=[Executable("rpgmakertrans_cli.py", base=base)],
+    options={'build_exe': build_exe_options},
+)
 
 if sys.platform == "win32":
     base = "Win32GUI"
 
 setup(
-    name="RPGMaker Trans" % version,
-    version="%s" version,
+    name="RPGMaker Trans %s" % version,
+    version="%s" % version,
     description="Translation tool for RPGMaker games",
-    executables=[Executable("rpgmakertrans_gui.py", base=base)])
+    executables=[Executable("rpgmakertrans_gui.py", base=base)],
+    options={'build_exe': build_exe_options},
+)
