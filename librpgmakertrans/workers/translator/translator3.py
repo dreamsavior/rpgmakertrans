66ea8268e7149017b335c6707373f930f182fda3
Aleph Fell <habisain@gmail.com>
2015-02-27 21:04:50 +0000

    Made a small edit to make sure comments are preserved during upgrading contexts; markers are discarded because they'll be regenerated anyway.
    --HG--
    branch : add-xp-vxace-support

--- librpgmakertrans/workers/translator/translator3.py
+++ librpgmakertrans/workers/translator/translator3.py
@@ -311,11 +311,11 @@ class TranslationFile:
         newLines = []
         for line in lines:
             if line.startswith('> CONTEXT:'):
-                tmp, _, markers = line.partition('<')[2]
-                context = tmp.partition(':')[2]
+                tmp, _, comment = line.partition('#')[2]
+                context = tmp.partition(':')[2].partition('<')[0]
                 parts = context.split('/')
                 newContext = '/'.join(parts[0] + (part for part in parts[1:] if part not in removeParts))
-                line = '> CONTEXT: %s%s' % (newContext, ('<%s' % markers) if markers else '')
+                line = '> CONTEXT: %s%s' % (newContext, ('#%s' % comment) if comment else '')
             newLines.append(line) 
         return newLines
     
