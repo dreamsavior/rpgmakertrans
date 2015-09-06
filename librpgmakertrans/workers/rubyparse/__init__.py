849ba70ad184c96565f4461c1d7f111b6312f355
Aleph Fell <habisain@gmail.com>
2015-09-06 22:16:05 +0100

    Fix a copyright date

--- librpgmakertrans/workers/rubyparse/__init__.py
+++ librpgmakertrans/workers/rubyparse/__init__.py
@@ -3,7 +3,7 @@ rubyparse
 *********
 
 :author: Aleph Fell <habisain@gmail.com>
-:copyright: 2012-2014
+:copyright: 2012-2015
 :license: GNU Public License version 3
 
 The Ruby parser. This is written in Python because Ruby1.8 lacks
