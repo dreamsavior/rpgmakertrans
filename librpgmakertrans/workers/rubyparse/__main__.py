35ea33c1f9501372c7a9fc637271d0c2eb1587a5
Aleph Fell <habisain@gmail.com>
2016-02-26 16:56:31 +0000

    New ruby parser is looking pretty good now, although may need tidying. I'm going to need a minor patch version bump to get to the new ones, and I'll probably need to be shipping the old parser in order to do it properly

--- librpgmakertrans/workers/rubyparse/__main__.py
+++ librpgmakertrans/workers/rubyparse/__main__.py
@@ -53,13 +53,13 @@ def test(string, verbose = None, filename = '', succeeds = True, errout=ErrorSen
         raise Exception('Succeeded when should have failed')
 
 heredoc = """
-  Content.new(:value => <<Eos
+  Content.new(:value => <<-Eos
     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
     incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
     exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
     irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
     pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-Eos
+  Eos
 )
 """
 if len(sys.argv) == 1:
